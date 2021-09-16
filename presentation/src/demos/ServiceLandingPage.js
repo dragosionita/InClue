e14cfceaa669ff9db637b3d1f9365532487f3450
Alina Gorgovan <alina@adoreme.com>
2021-09-16 17:15:34 +0300

    first commit presentation site

---
+++ presentation/src/demos/ServiceLandingPage.js
@@ -0,0 +1,83 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+import Hero from "components/hero/TwoColumnWithInput.js";
+import Features from "components/features/ThreeColWithSideImage.js";
+import MainFeature from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+import FeatureStats from "components/features/ThreeColCenteredStatsPrimaryBackground.js";
+import Pricing from "components/pricing/TwoPlansWithDurationSwitcher.js";
+import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
+import FAQ from "components/faqs/SingleCol.js";
+import GetStarted from "components/cta/GetStartedLight.js";
+import Footer from "components/footers/MiniCenteredFooter.js";
+
+import EasyInstallImg from "../images/setup.svg";
+
+
+const HighlightedText = tw.span`text-blue-500`;
+
+export default () => {
+  return (
+    <AnimationRevealPage>
+      <Hero />
+      <FeatureStats />
+      <Features
+        heading={
+          <>
+            {" "}
+            Amazing <HighlightedText> Features </HighlightedText>{" "}
+          </>
+        }
+      />{" "}
+      <MainFeature
+        heading={
+          <>
+            {" "}
+            How does it work?
+            <HighlightedText> Automatically </HighlightedText>{" "}
+          </>
+        }
+      />{" "}
+      <MainFeature
+        imageSrc={EasyInstallImg}
+        subheading={<>Integrate With Any Website</>}
+        textOnLeft={false}
+        heading={
+          <>
+            {" "}
+            Easily installed in <HighlightedText>2-minutes</HighlightedText> on any CMS or web hosting platform
+          </>
+        }
+      />{" "}
+      <Testimonial
+        textOnLeft={true}
+        heading={
+          <>
+            {" "}
+            Our Clients <HighlightedText> Love Us </HighlightedText>{" "}
+          </>
+        }
+      />{" "}
+      <Pricing
+        heading={
+          <>
+            {" "}
+            Ready to Become <HighlightedText> Accessible </HighlightedText> ? {" "}
+          </>
+        }
+      />{" "}
+      <FAQ
+        heading={
+          <>
+            {" "}
+            <HighlightedText> FAQ </HighlightedText>{" "}
+          </>
+        }
+      />{" "}
+      <GetStarted />
+      <Footer />
+    </AnimationRevealPage>
+  );
+};
